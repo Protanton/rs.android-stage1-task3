399823c93fd4d9dd4a083cb61d6aa35f362c6c15
Protanton <protanton@gmail.com>
2020-05-05 12:11:22 +0300

    feat: subtask2 solve

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,20 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var c = array[0]
+        var n = array[1]
+        var answer = 0
+        fun factorial(a: Int): Long = if (a == 1) a.toLong() else factorial(a - 1) * a
+        for(k in n-1 downTo 1){
+            var total = factorial(n) / (factorial(n - k) * factorial(k))
+            if(total.toInt() == c) {
+                answer = k
+            }
+        }
+        return if (answer != 0) {
+            answer
+        } else {
+            null
+        }
     }
 }
